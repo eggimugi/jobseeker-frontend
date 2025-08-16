226286ef901ea19de4ebb54c56a316dcaa992f8f
eggi mugi <eggimugi03@gmail.com>
2025-08-16 22:46:26 +0700

    first commit

--- prisma/migrations/20250816123427_change_status_type_in_position_applied_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `status` on the `position_applied` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Enum(EnumId(1))`.
-
-*/
--- AlterTable
-ALTER TABLE `position_applied` MODIFY `status` ENUM('PENDING', 'ACCEPTED', 'REJECTED') NOT NULL;
